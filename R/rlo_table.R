5ff137d38c198e9c581541d326a8da729eda2792
Johannes Ranke <jranke@uni-bremen.de>
2015-01-07 08:04:01 +0100

    Support for merged group header cells

--- R/rlo_table.R
+++ R/rlo_table.R
@@ -1,5 +1,8 @@
 rlo_table <- function(x, captiontext, 
-                      header = "colnames", footer = NULL,
+                      header = "colnames", 
+                      group_header = NULL,
+                      group_sizes = NULL,
+                      footer = NULL,
                       factors = NULL, merge_index = NULL,
                       numbered = TRUE, 
                       break_before_caption = FALSE,
@@ -21,8 +24,23 @@ rlo_table <- function(x, captiontext,
   if (header[1] == "colnames") {
     header <- colnames(x)
   }
+
+  n_headrows = 0
   if (!is.null(header)) {
     x <- rbind(header, x)
+    n_headrows = 1
+    if (!is.null(group_header)) {
+      group_header_expanded = NULL
+      for (group in seq_along(group_header)) {
+        group_header_expanded = c(group_header_expanded, group_header[group])
+        group_size = group_sizes[group]
+        if (group_size > 1) {
+          group_header_expanded = c(group_header_expanded, rep("", group_size - 1))
+        }
+      }
+      x <- rbind(group_header_expanded, x)
+      n_headrows = 2
+    }
   }
 
   mergelist = list()
@@ -63,10 +81,17 @@ rlo_table <- function(x, captiontext,
 
   if (!is.null(factors)) {
     for (i in length(factors):1) {
-      x <- cbind(c(names(factors)[i], factors[[i]]), x)
+      if (n_headrows == 0) {
+        x <- cbind(factors[[i]], x)
+      }
+      if (n_headrows == 1) {
+        x <- cbind(c(names(factors)[i], factors[[i]]), x)
+      }
+      if (n_headrows == 2) {
+        x <- cbind(c(names(factors)[i], "", factors[[i]]), x)
+      }
     }
   }
-
 
   dimnames(x) <- NULL
 
@@ -76,16 +101,36 @@ rlo_table <- function(x, captiontext,
   python.exec("x = tuple(tuple(i) for i in x)")
   python.exec("tbl.setDataArray(x)")
 
+  # Merge factor columns if requested
   for (factor_col in names(mergelist)) {
     if (factor_col %in% merge_index) {
       for (ei in seq_along(mergelist[[factor_col]])) {
         entry = mergelist[[factor_col]][[ei]]
-        python.assign("cellname", paste0(factor_col, entry["start"] + 1))
+        python.assign("cellname", paste0(factor_col, entry["start"] + n_headrows))
         python.exec("tcursor = tbl.createCursorByCellName(cellname)")
         stepsize = entry["end"] - entry["start"]
         python.exec(paste0("tcursor.goDown(", stepsize, ", True)"))
         python.exec("tcursor.mergeRange()")
       }
+    }
+  }
+
+  # Merge headers of factor columns and group header fields if group header is present
+  if (!is.null(group_header)) {
+    for (factor_index in seq_along(factors)) {
+      factor_col = LETTERS[factor_index]    
+      python.assign("cellname", paste0(factor_col, 1))
+      python.exec("tcursor = tbl.createCursorByCellName(cellname)")
+      python.exec(paste0("tcursor.goDown(1, True)"))
+      python.exec("tcursor.mergeRange()")
+    }
+    col_index = length(factors)
+    for (group_size in group_sizes) {
+      col_index = col_index + 1
+      python.assign("cellname", paste0(LETTERS[col_index], 1))
+      python.exec("tcursor = tbl.createCursorByCellName(cellname)")
+      python.exec(paste0("tcursor.goRight(", group_size - 1, ", True)"))
+      python.exec("tcursor.mergeRange()")
     }
   }
 
@@ -99,9 +144,8 @@ rlo_table <- function(x, captiontext,
     python.exec("scursor.setPropertyValue('ParaStyleName', 'Textkörper mit Abstand')")
   }
 
-  if (first_column_width != "default") {
-    python.exec("tcursor = tbl.createCursorByCellName('A1')")
-    
-    
-  }
+  # Sizing of columns not implemented
+#   if (first_column_width != "default") {
+#     python.exec("tcursor = tbl.createCursorByCellName('A1')")
+#   }
 }
