1010e5e259cd4948dd75f1d058cbbe1f60bf1220
Johannes Ranke <jranke@uni-bremen.de>
2015-10-01 18:04:03 +0200

    Add incomplete finalizer

---
+++ R/rlo_finalize.R
@@ -0,0 +1,9 @@
+#' Function to finalize a jrwb report
+#'
+#' Not working yet, therefore not exported
+#'
+#' @importFrom PythonInR pyExec
+rlo_finalize <- function()
+{
+  rlo_dispatch(".uno:UpdateAll") 
+}
